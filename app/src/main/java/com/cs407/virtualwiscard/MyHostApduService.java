0be82591e75282365bd2cefbf007325f61d6633d
Jake Garneau <jgarneau@wisc.edu>
2024-12-08 23:57:42 -0600

    phone sends correct message!!

---
+++ app/src/main/java/com/cs407/virtualwiscard/MyHostApduService.java
@@ -0,0 +1,63 @@
+package com.cs407.virtualwiscard;
+
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MyHostApduService extends HostApduService {
+
+    private static final String SELECT_APDU_HEADER = "00A40400"; // APDU command for SELECT
+    private static final String RESPONSE_ERROR = "6F00"; // Error response
+
+    @Override
+    public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
+        Log.d("HCE", "Received APDU: " + bytesToHex(apdu));
+        String apduHex = bytesToHex(apdu);
+
+        if (apduHex.startsWith(SELECT_APDU_HEADER)) {
+            // Handle the SELECT command
+            String message = "brawlstars";
+            byte[] responseBytes = message.getBytes();
+            Log.d("HCE", "APDU response: " + bytesToHex(responseBytes));
+            return responseBytes;
+        }
+
+        // Return error if command is not recognized
+        return hexStringToByteArray(RESPONSE_ERROR);
+    }
+
+    @Override
+    public void onDeactivated(int reason) {
+        Log.d("HCE", "onDeactivated called with reason: " + reason);
+        switch (reason) {
+            case DEACTIVATION_LINK_LOSS:
+                Log.d("HCE", "Deactivation due to link loss.");
+                break;
+            case DEACTIVATION_DESELECTED:
+                Log.d("HCE", "Deactivation due to deselection.");
+                break;
+            default:
+                Log.d("HCE", "Deactivation for unknown reason.");
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02X", b));
+        }
+        return result.toString();
+    }
+
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+}
+
+
